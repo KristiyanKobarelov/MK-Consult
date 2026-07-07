38c3ca573d88fa22bfc23a425754d2bebddaa282
Kristiyan Kobarelov <123277206+KristiyanKobarelov@users.noreply.github.com>
2026-07-07 21:07:56 +0300

    Spring Boot workspace

---
+++ mkconsult/src/main/java/com/maria/mkconsult/MkconsultApplication.java
@@ -0,0 +1,13 @@
+package com.maria.mkconsult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkconsultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkconsultApplication.class, args);
+	}
+
+}
---
+++ mkconsult/src/test/java/com/maria/mkconsult/MkconsultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maria.mkconsult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkconsultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
